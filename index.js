d12a7e6216845b1a903cb511cb8c7b92e13d07c9
Tushar <developer.tks@gmail.com>
2017-12-25 00:21:10 +0530

    index.js file with proper sample code

---
+++ index.js
@@ -0,0 +1,10 @@
+// requiring the HTTP interfaces in node
+var http = require('http');
+
+//Creating and http server to handle requests and response
+http.createServer(function (req, res){
+	res.writeHead(200,{'Content-Type':'text/plain'})
+	res.end('Hello World. I am here');
+}).listen(8080);
+
+console.log('Server running on port:8080');
