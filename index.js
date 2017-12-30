8764871454084b3495e2f37794f3af67f6616476
Tushar <developer.tks@gmail.com>
2017-12-31 01:32:54 +0530

    new change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // requiring the HTTPS interfaces in node
 var https = require('https');
-
+var port = process.env.PORT || 3000
 //Creating and http server to handle requests and response
 https.createServer(function (req, res){
 	res.writeHead(200,{'Content-Type':'text/plain'})
 	res.end('Hello World. I am here');
-}).listen(8080);
+}).listen(port);
 
 
-console.log('Server running on port:8080'); 
+console.log('Server running on port:port'); 
