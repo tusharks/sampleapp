ba2b8e5a8e6ae350f866780cd645201af41bc074
Tushar <developer.tks@gmail.com>
2017-12-31 01:20:10 +0530

    new change

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// requiring the HTTP interfaces in node
-var http = require('http');
+// requiring the HTTPS interfaces in node
+var https = require('https');
 
 //Creating and http server to handle requests and response
-http.createServer(function (req, res){
+https.createServer(function (req, res){
 	res.writeHead(200,{'Content-Type':'text/plain'})
 	res.end('Hello World. I am here');
-}).listen(5000);
+}).listen(8080);
 
+http.listen(process.env.PORT || 8080, function(){
+  console.log('listening on', http.address().port);
+});
 console.log('Server running on port:8080'); 
