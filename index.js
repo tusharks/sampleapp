95452dfa7413e570e877b8c8537da8a0416a488a
Tushar <developer.tks@gmail.com>
2018-01-01 01:29:05 +0530

    new change

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 //Creating and http server to handle requests and response
-// http.createServer(function (req, res){
-// 	res.writeHead(200,{'Content-Type':'text/plain'})
-// 	res.end('Hello World. I am here');
-// }).listen(port);
-app.get("/",function(req,res){
-	res.send("Welcome to Node JS on Heroku");	
-});
-app.listen(port);
+http.createServer(function (req, res){
+	res.writeHead(200,{'Content-Type':'text/plain'})
+	res.end('Hello World. I am here');
+}).listen(port);
+// app.get("/",function(req,res){
+// 	res.send("Welcome to Node JS on Heroku");	
+// });
+// app.listen(port);
 console.log('Server running on port:',port); 
