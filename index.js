5742e18c0796a039ed34a3838ef49911d1094bba
Tushar <developer.tks@gmail.com>
2018-01-01 01:25:26 +0530

    new change

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // requiring the HTTPS interfaces in node
 var http = require('https');
-var port = process.env.PORT || 3000
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 //Creating and http server to handle requests and response
-http.createServer(function (req, res){
-	res.writeHead(200,{'Content-Type':'text/plain'})
-	res.end('Hello World. I am here');
-}).listen(port);
-
-
-console.log('Server running on port:port'); 
+// http.createServer(function (req, res){
+// 	res.writeHead(200,{'Content-Type':'text/plain'})
+// 	res.end('Hello World. I am here');
+// }).listen(port);
+app.get("/",function(req,res){
+	res.send("Welcome to Node JS on Heroku");	
+});
+app.listen(port);
+console.log('Server running on port:',port); 
